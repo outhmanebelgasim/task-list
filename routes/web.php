c5f2f93303bd52eb9ad8cce3b25dc6274aed2d2a
Belgasim Outhmane <125576314+outhmanebelgasim@users.noreply.github.com>
2026-03-15 16:02:50 +0000

    update: adding dashboard reoute

--- routes/web.php
+++ routes/web.php
@@ -5,15 +5,16 @@ use Laravel\Fortify\Features;
 use App\Http\Controllers\ListController;
 use Inertia\Inertia;
 use App\Http\Controllers\TaskController;
+use App\Http\Controllers\DashboardController;
 
 Route::inertia('/', 'welcome', [
     'canRegister' => Features::enabled(Features::registration()),
 ])->name('home');
 
 Route::middleware(['auth', 'verified'])->group(function () {
+    Route::get('/dashboard', [DashboardController::class, 'index'])->name('dashboard');
     Route::resource("lists", ListController::class);
     Route::resource("tasks", TaskController::class);
-    Route::inertia('dashboard', 'dashboard')->name('dashboard');
 });
 
 require __DIR__.'/settings.php';
